3f02e477e561d2b0487a92975bc3a1c03efff4bc
kelvinlins <48449392+kelvinlins@users.noreply.github.com>
2024-12-03 21:16:24 -0300

    Update k8s_deployment.tf

--- terraform/infra/k8s_deployment.tf
+++ terraform/infra/k8s_deployment.tf
@@ -4,6 +4,7 @@ data "aws_ecr_repository" "repository" {
 
 
 resource "kubernetes_deployment" "deployment" {
+  depends_on = [kubernetes_secret.token_secret]
   metadata {
     name = "${var.project_name}-deployment"
   }
